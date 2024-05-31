8c256139cf483237b4ebf651d5cc7bded6bc1840
Gabriel Fernandes <gabriel.fernandes.06@outlook.com>
2024-05-31 18:15:52 -0300

    :lipstick: ui: adding application icons

--- src/components/ui/shared/Icon/Icon.tsx
+++ src/components/ui/shared/Icon/Icon.tsx
@@ -1,19 +1,41 @@
 import { HTMLAttributes } from 'react'
 import { icons } from './Icons'
+import { tv } from 'tailwind-variants'
 
 interface IconProps extends HTMLAttributes<SVGElement> {
   name: keyof typeof icons
+  label?: string
   width?: string
   height?: string
   className?: string
 }
 
-export function Icon({ name, width, height, className, ...props }: IconProps) {
+const iconDiv = tv({
+  base: 'flex items-center justify-center gap-2',
+})
+
+const labelText = tv({
+  base: 'text-sm text-slate-500',
+})
+
+export function Icon({
+  name,
+  label,
+  width,
+  height,
+  className,
+  ...props
+}: IconProps) {
   const Icon = icons[name]
 
   if (!Icon) {
     return null
   }
 
-  return <Icon width={width} height={height} className={className} {...props} />
+  return (
+    <div className={iconDiv()}>
+      {label && <p className={labelText()}>{label}</p>}
+      <Icon width={width} height={height} className={className} {...props} />
+    </div>
+  )
 }
--- src/components/ui/shared/Icon/Icons/index.ts
+++ src/components/ui/shared/Icon/Icons/index.ts
@@ -3,6 +3,16 @@ import { Menu } from './Menu'
 import { X } from './X'
 import { GitHub } from './GitHub'
 import { Linkedin } from './Linkedin'
+import { Comment } from './Comment'
+import { Star } from './Star'
+import { Terminal } from './Terminal'
+import { Joystick } from './Joystick'
+import { Trophy } from './Trophy'
+import { Folder } from './Folder'
+import { Markdown } from './Markdown'
+import { Email } from './Email'
+import { Phone } from './Phone'
+import { React } from './React'
 
 export interface IconProps extends HTMLAttributes<SVGElement> {
   width?: string
@@ -15,4 +25,14 @@ export const icons = {
   menu: Menu,
   github: GitHub,
   linkedin: Linkedin,
+  comment: Comment,
+  star: Star,
+  terminal: Terminal,
+  joystick: Joystick,
+  trophy: Trophy,
+  folder: Folder,
+  markdown: Markdown,
+  email: Email,
+  phone: Phone,
+  react: React,
 }
